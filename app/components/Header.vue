f1db95edf47efa2ff3ceb51ba50e2b833d90db8e
zyadwael1 <zyad.wael@34ml.com>
2026-01-25 11:06:20 +0200

    fix:fixing configs

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,5 +1,13 @@
 <template>
-Header
+
+    <div>
+        <header>
+            <div class="bg-blue-600 text-black p-4">
+                <h2 class="text-2xl font-bold">Header Component</h2>
+            </div>
+        </header>
+    </div>
+
 
 </template>
 <script setup lang="ts"></script>
