2d1ad52a784108585093834e27cb30581fcfc928
zyadwael1 <zyad.wael@34ml.com>
2026-01-26 13:58:00 +0200

    chore:applying some code standards

--- app/components/ProductsList.vue
+++ app/components/ProductsList.vue
@@ -1,26 +1,37 @@
 <template>
-    <div class="products-list-back-container flex justify-center items-center h-[49px] bg-[#F9F9F9] border-b-[0.5px] border-[#DFE1E3] ">
-    <ul class="w-[80%] flex justify-around">
-        <li 
-        v-for="product in products"
-        class=""
-        >
-            <NuxtLink to='product-path'>{{ product }}</NuxtLink>
-        </li>
+  <div class="products-list-back-container">
+    <ul class="products-list">
+      <li 
+      v-for="product in products"
+      :key="product.id"
+      >
+        <NuxtLink to="product-path">{{ product.name }}</NuxtLink>
+      </li>
     </ul>
-    </div>
+  </div>
 </template>
 <script setup lang="ts">
-const products = {
-    1 :"Air conditioners",
-    2 :"Washing Machines",
-    3 :"Television",
-    4 :"Fans",
-    5 :"Water Heater",
-    6 :"Water Filters",
-    7 :"Refrigerator",
-    8 :"Vacuum Cleaner",
-    9 :"Brands",
+interface Product {
+  id: number;
+  name: string;
 }
+const products: Product[] = [
+  { id: 1, name: "Air conditioners" },
+  { id: 2, name: "Washing Machines" },
+  { id: 3, name: "Television" },
+  { id: 4, name: "Fans" },
+  { id: 5, name: "Water Heater" },
+  { id: 6, name: "Water Filters" },
+  { id: 7, name: "Refrigerator" },
+  { id: 8, name: "Vacuum Cleaner" },
+  { id: 9, name: "Brands" },
+];
 </script>
-<style scoped></style>
+<style scoped>
+.products-list-back-container {
+  @apply flex justify-center items-center h-[49px] bg-[#F9F9F9] border-y-[0.5px] border-[#DFE1E3];
+}
+.products-list {
+  @apply w-[80%] flex justify-around;
+}
+</style>
