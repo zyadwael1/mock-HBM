b346efe8a93d26b3fb99b3297c834c522b0f253d
zyadwael1 <zyad.wael@34ml.com>
2026-02-03 01:04:08 +0200

    feat:add account page

---
+++ app/pages/account.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col items-center">
+    <h2 class="p-20 text-4xl">My Account</h2>
+    <BaseButton text="Logout" class="!w-28" />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
