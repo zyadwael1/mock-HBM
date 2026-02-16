8834ce7daa1ff98d0afb40e22b5c427bb7567b48
AlhoussinAmin <Alhoussin@34ml.com>
2026-02-16 03:21:25 +0200

    chore: rename product grid

---
+++ app/components/products/Grid/Skeleton.vue
@@ -0,0 +1,5 @@
+<template>
+  <div
+    class="flex min-h-[260px] w-[154px] flex-col gap-3 rounded-xl bg-gray-400 md:min-h-[420px] md:w-[250px]"
+  ></div>
+</template>
---
+++ app/components/products/Grid/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="flex flex-row flex-wrap justify-center gap-4 md:justify-center md:gap-7 md:px-48 md:py-9"
+  >
+    <template v-if="!products">
+      <Skeleton v-for="_ in 10" />
+    </template>
+
+    <h2 v-else-if="products.length === 0">
+      We couldn't find an exact match to what you are looking for.
+    </h2>
+
+    <template v-else>
+      <GridItem v-for="product in products" :product />
+    </template>
+  </div>
+</template>
+
+<script setup lang="ts">
+import GridItem from "~/components/products/Grid/item.vue";
+import Skeleton from "~/components/products/Grid/Skeleton.vue";
+import type { ProductType } from "~/types/types";
+
+defineProps<{
+  products?: ProductType[];
+}>();
+</script>
